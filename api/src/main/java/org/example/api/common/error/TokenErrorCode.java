e702b9bf849e9172ca307505e217c7f8a43ea4f8
yys <dgh06008@naver.com>
2024-06-25 18:50:45 +0900

    Feat: AUTHORIZATION TOKEN NOT FOUND error 추가

--- api/src/main/java/org/example/api/common/error/TokenErrorCode.java
+++ api/src/main/java/org/example/api/common/error/TokenErrorCode.java
@@ -11,7 +11,8 @@ import lombok.Getter;
 public enum TokenErrorCode implements ErrorCodeIfs{
     INVALID_TOKEN(400,2000,"유효하지 않는 토큰"),
     EXPIRED_TOKEN(400,2001,"만료된 토큰"),
-    TOKEN_EXCEPTION(400,2002,"알 수 없는 토큰 에러")
+    TOKEN_EXCEPTION(400,2002,"알 수 없는 토큰 에러"),
+    AUTHORIZATION_TOKEN_NOT_FOUND(400,2003,"인증 헤더 토큰 없음")
 
     ;
 
