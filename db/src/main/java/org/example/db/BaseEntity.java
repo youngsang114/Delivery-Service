2a847b49ddfa0cedfbac58e2e8c4759a388b3f3b
yys <dgh06008@naver.com>
2024-06-25 15:17:43 +0900

    Feat: 생성자 어노테이션 추가

--- db/src/main/java/org/example/db/BaseEntity.java
+++ db/src/main/java/org/example/db/BaseEntity.java
@@ -4,12 +4,16 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @MappedSuperclass
 @Data
 @SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
 public class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
