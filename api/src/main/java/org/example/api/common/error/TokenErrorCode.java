9904cf9a534f5f576c006e22dce77251b56ac815
yys <dgh06008@naver.com>
2024-06-25 18:04:35 +0900

    Feat: TokenErrorCode 추가

---
+++ api/src/main/java/org/example/api/common/error/TokenErrorCode.java
@@ -0,0 +1,21 @@
+package org.example.api.common.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * USER의 경우 2000번대 에러코드 사용
+ */
+@Getter
+@AllArgsConstructor
+public enum TokenErrorCode implements ErrorCodeIfs{
+    INVALID_TOKEN(400,2000,"유효하지 않는 토큰"),
+    EXPIRED_TOKEN(400,2001,"만료된 토큰"),
+    TOKEN_EXCEPTION(400,2002,"알 수 없는 토큰 에러")
+
+    ;
+
+    private final Integer httpStatusCode; // 상응 하는 http error code
+    private final Integer errorCode; // 인터널 error code
+    private final String description;
+}
