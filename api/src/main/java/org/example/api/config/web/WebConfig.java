993c476a31e18b308b990df1a40639d2dc142a03
yys <dgh06008@naver.com>
2024-06-25 14:13:55 +0900

    Feat: Interceptor config 설정, 기본 제외 uri 추가

---
+++ api/src/main/java/org/example/api/config/web/WebConfig.java
@@ -0,0 +1,42 @@
+package org.example.api.config.web;
+
+import lombok.RequiredArgsConstructor;
+import org.example.api.interceptor.AuthorizationInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthorizationInterceptor authorizationInterceptor;
+
+    private List<String> OPEN_API = List.of(
+            "/open-api/**"
+    );
+
+    private List<String> DEFAULT_EXCLUDE = List.of(
+            "/",
+            "favicon.ico",
+            "/error"
+    );
+
+    private List<String> SWAGGER = List.of(
+            "/swagger-ui.html",
+            "/swagger-ui/**",
+            "/v3/api-docs/**"
+    );
+
+    // [open-api] 로 시작하는 uri는 검증을 하지 않는다
+    // [api] 로 시작하는 uri는 검증을 할 것이다
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authorizationInterceptor)
+                .excludePathPatterns(OPEN_API)
+                .excludePathPatterns(DEFAULT_EXCLUDE)
+                .excludePathPatterns(SWAGGER);
+    }
+}
