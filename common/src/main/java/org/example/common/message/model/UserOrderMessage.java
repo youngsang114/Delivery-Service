25901f6f7946a706c318215666e79672031c0451
yys <dgh06008@naver.com>
2024-06-30 23:35:38 +0900

    Feat: Rabbit message의 ãproducer 과 consumer 간의 공통 객체 설정 모듈

---
+++ common/src/main/java/org/example/common/message/model/UserOrderMessage.java
@@ -0,0 +1,16 @@
+package org.example.common.message.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserOrderMessage {
+
+    private Long userOrderId;
+}
